45aa2c992f1b9bc0d209037013938866b74798a7
syahdaafia <syahdaafia@gmail.com>
2022-11-09 09:45:32 +0700

    up frontend

---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,47 @@
+<script setup>
+import AnalyticsAward from '@/views/dashboards/analytics/AnalyticsAward.vue'
+import AnalyticsSalesByCountries from '@/views/dashboards/analytics/AnalyticsSalesByCountries.vue'
+import AnalyticsTotalEarning from '@/views/dashboards/analytics/AnalyticsTotalEarning.vue'
+import AnalyticsTransactions from '@/views/dashboards/analytics/AnalyticsTransactions.vue'
+import AnalyticsWeeklyOverview from '@/views/dashboards/analytics/AnalyticsWeeklyOverview.vue'
+</script>
+
+<template>
+  <VRow class="match-height">
+    <VCol
+      cols="12"
+      md="4"
+    >
+      <AnalyticsAward />
+    </VCol>
+
+    <VCol
+      cols="12"
+      md="4"
+    >
+      <AnalyticsTransactions />
+    </VCol>
+
+    <VCol
+      cols="12"
+      md="4"
+    >
+      <AnalyticsWeeklyOverview />
+    </VCol>
+
+    <VCol
+      cols="12"
+      md="8"
+    >
+      <AnalyticsTotalEarning />
+    </VCol>
+
+    <VCol
+      cols="12"
+      md="4"
+    >
+      <AnalyticsSalesByCountries />
+    </VCol>
+
+  </VRow>
+</template>
---
+++ frontend/src/views/dashboards/analytics/AnalyticsTotalEarning.vue
@@ -0,0 +1,77 @@
+<script setup>
+import aviato from '@/assets/images/logos/aviato.png'
+import bitbank from '@/assets/images/logos/bitbank.png'
+import zipcar from '@/assets/images/logos/zipcar.png'
+
+const earnings = [
+  {
+    avatar: zipcar,
+    title: '17 Oktober 2022',
+    subtitle: 'Created Journal 3',
+    amount: '',
+    progress: 'primary',
+  },
+  {
+    avatar: bitbank,
+    title: '28 September 2022',
+    subtitle: 'Created Journal 2',
+    amount: '',
+    progress: 'info',
+  },
+  {
+    avatar: aviato,
+    title: '1 September 2022',
+    subtitle: 'Created Journal 1',
+    amount: '',
+    progress: 'secondary',
+  },
+]
+</script>
+
+<template>
+  <VCard>
+    <VCardItem>
+      <h2>Timeline</h2>
+    </VCardItem>
+    <VCardText class="pt-4">
+      <VList class="card-list mt-9">
+        <VListItem
+          v-for="earning in earnings"
+          :key="earning.title"
+        >
+          <template #prepend>
+            <VAvatar
+              rounded
+              :image="earning.avatar"
+            />
+          </template>
+
+          <VListItemTitle class="text-sm font-weight-semibold mb-1">
+            {{ earning.title }}
+          </VListItemTitle>
+          <VListItemSubtitle class="text-xs">
+            {{ earning.subtitle }}
+          </VListItemSubtitle>
+
+          <template #append>
+            <div>
+              <h6 class="text-sm mb-2">
+                {{ earning.amount }}
+              </h6>
+              <VProgressLinear
+                :color="earning.progress"
+                model-value="80"
+              />
+            </div>
+          </template>
+        </VListItem>
+      </VList>
+    </VCardText>
+  </VCard>
+</template>
+
+<style lang="scss" scoped>
+.card-list {
+  --v-card-list-gap: 2.625rem;
+}
+</style>
